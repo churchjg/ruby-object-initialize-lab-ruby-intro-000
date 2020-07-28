2c9a8660928c5af60a2d67bd3ce028165f00e717
Jonathan Church <churchjg283@gmail.com>
2020-07-28 20:36:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
+© 2020 GitHub, Inc.
